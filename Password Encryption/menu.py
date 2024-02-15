251a98755200aba92ba01fb584a54786b5bab875
Ali <36553593+aad3m@users.noreply.github.com>
2024-02-15 16:32:55 -0600

    Update menu.py
    Minor Changes

--- Password Encryption/menu.py
+++ Password Encryption/menu.py
@@ -1,32 +1,27 @@
+''' Interactive Menu To Access Password Encryption Options'''
 from filecheck import check_user_database
+
 try:
     check_user_database()
 except ValueError as e:
     print(f'Errors were found: {e}')
     exit()
 
-
 import time
 from database import add_account
 from login import login
 from customize import color
 
-# Constants
-LOGIN_CHOICE = '1'
-REGISTER_CHOICE = '2'
-ABOUT_CHOICE = '3'
-EXIT_CHOICE = '4'
-
 def display_menu():
-    """Display the menu options."""
     print(f"{color.GREEN}\nMenu:"
-          f"\n{LOGIN_CHOICE}. Login"
-          f"\n{REGISTER_CHOICE}. Register"
-          f"\n{ABOUT_CHOICE}. About"
-          f"\n{EXIT_CHOICE}. Exit {color.END}")
+          f"\n1. Login"
+          f"\n2. Register"
+          f"\n3. About"
+          f"\n4. Exit {color.END}")
 
-def about():
-    """Display information about the program."""
+
+
+def about():  # about section
     print(f"\n {color.BOLD}Password Encryption: {color.END}"
           f"{color.CYAN}\n- Encrypts Username & Password "
           "\n- Menu Option "
@@ -35,27 +30,29 @@ def about():
           "\n- File Checker"
           f"\n- User Database {color.END}")
 
+
 if __name__ == '__main__':
 
     while True:
         print(f"{color.YELLOW}\nWelcome to the Password Encryption by aad3m! {color.END}")
         display_menu()
 
-        choice = input(f"{color.BOLD}Enter your choice ({LOGIN_CHOICE}/{REGISTER_CHOICE}/{ABOUT_CHOICE}/{EXIT_CHOICE}): {color.END}")
+        choice = input(f"{color.BOLD}Enter your choice (1/2/3/4): {color.END}")
 
-        if choice == LOGIN_CHOICE:
+        if choice == '1':
             login()
             time.sleep(2)
-        elif choice == REGISTER_CHOICE:
+        elif choice == '2':
             result = add_account()
             if result is not None:
+                new_user, new_password = result
                 time.sleep(3)
-        elif choice == ABOUT_CHOICE:
+        elif choice == '3':
             about()
             time.sleep(3)
-        elif choice == EXIT_CHOICE:
+        elif choice == "4":
             print(f"{color.YELLOW}Thank you for using Password Encryption by aad3m {color.END}")
             break
         else:
-            print(f"{color.RED}Invalid choice. Please enter {LOGIN_CHOICE}, {REGISTER_CHOICE}, {ABOUT_CHOICE}, or {EXIT_CHOICE}. {color.END}")
+            print(f"{color.RED}Invalid choice. Please enter 1, 2, 3, or 4. {color.END}")
             time.sleep(1.5)
